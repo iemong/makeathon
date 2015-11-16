7710cf12b307123d0f35fe8a45048809241681fc
入江信之介 <(kiyoshidainagon@gmail.com)>
2015-11-16 12:50:20 +0900

    残り作業 : サムネ画像を追加。映像の制作。表示画面のオーサリング。

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var socketIO = require('socket.io');
-
+var json = require('../list.json');
+var nameList = [];
+var imageList = [];
+var urlList = [];
+var captionList = [];
+for(var i = 0; i < Object.keys(json.movieList).length; i++){
+    nameList.push(json.movieList[i].name);
+    imageList.push(json.movieList[i].image);
+    urlList.push(json.movieList[i].url);
+    captionList.push(json.movieList[i].caption);
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -10,17 +20,29 @@ router.get('/', function(req, res, next) {
 });
 router.get('/page1', function(req, res, next){
     res.render('top_parts', {
-        title: 'page1'
+        title:    'page1',
+        names:    nameList,
+        images:   imageList,
+        urls:     urlList,
+        captions: captionList
     });
 })
 router.get('/page2', function(req, res, next){
     res.render('top_parts', {
-        title: 'page2'
+        title: 'page2',
+        names:    nameList,
+        images:   imageList,
+        urls:     urlList,
+        captions: captionList
     });
 })
 router.get('/page3', function(req, res, next){
     res.render('top_parts', {
-        title: 'page3'
+        title: 'page3',
+        names:    nameList,
+        images:   imageList,
+        urls:     urlList,
+        captions: captionList
     });
 })
 
