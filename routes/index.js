140f6b75c3fcb780ddb0e530d541fb44e4fed788
入江信之介 <(kiyoshidainagon@gmail.com)>
2015-11-20 19:40:01 +0900

    画像を入れる前に保存

--- routes/index.js
+++ routes/index.js
@@ -10,45 +10,88 @@ var nameList = [];
 var imageList = [];
 var urlList = [];
 var captionList = [];
+var imageNumList = [];
 for(var i = 0; i < Object.keys(json.movieList).length; i++){
     nameList.push(json.movieList[i].name);
     imageList.push(json.movieList[i].image);
     urlList.push(json.movieList[i].url);
     captionList.push(json.movieList[i].caption);
+    imageNumList.push(json.movieList[i].imageNum);
 }
 
 /* GET home page. */
+
+var items = 5;
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'contoroller'
+      title: 'contoroller',
+      number: items
   });
 });
+router.get('/page0', function(req, res, next){
+    res.render('top_parts', {
+        title:     'page0',
+        names:     nameList,
+        images:    imageList,
+        imageNums: imageNumList,
+        urls:      urlList,
+        captions:  captionList
+    });
+})
 router.get('/page1', function(req, res, next){
     res.render('top_parts', {
-        title:    'page1',
-        names:    nameList,
-        images:   imageList,
-        urls:     urlList,
-        captions: captionList
+        title:     'page1',
+        names:     nameList,
+        images:    imageList,
+        imageNums: imageNumList,
+        urls:      urlList,
+        captions:  captionList
     });
 })
 router.get('/page2', function(req, res, next){
     res.render('top_parts', {
-        title: 'page2',
-        names:    nameList,
-        images:   imageList,
-        urls:     urlList,
-        captions: captionList
+        title:     'page2',
+        names:     nameList,
+        images:    imageList,
+        imageNums: imageNumList,
+        urls:      urlList,
+        captions:  captionList
     });
 })
 router.get('/page3', function(req, res, next){
     res.render('top_parts', {
-        title: 'page3',
-        names:    nameList,
-        images:   imageList,
-        urls:     urlList,
-        captions: captionList
+        title:     'page3',
+        names:     nameList,
+        images:    imageList,
+        imageNums: imageNumList,
+        urls:      urlList,
+        captions:  captionList
     });
 })
+router.get('/page4', function(req, res, next){
+    res.render('top_parts', {
+        title:     'page4',
+        names:     nameList,
+        images:    imageList,
+        imageNums: imageNumList,
+        urls:      urlList,
+        captions:  captionList
+    });
+})
+router.get('/movie/page1', function(req, res, next) {
+    res.render('movie_show', {
+        title: 'page1'
+    });
+});
+router.get('/movie/page2', function(req, res, next) {
+    res.render('movie_show', {
+        title: 'page2'
+    });
+});
+router.get('/movie/page3', function(req, res, next) {
+    res.render('movie_show', {
+        title: 'page3'
+    });
+});
 
 module.exports = router;
